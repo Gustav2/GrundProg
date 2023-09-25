ec06c6cfd9337a818026dd819987295e39aa2c3f
Gustav Søndergaard Nybro <gustav@gustavnybro.com>
2023-09-25 13:22:29 +0200

    added opg2

---
+++ 2_lektion/opg2.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+t = np.arange(0, 10)
+print(t)
+
+print(np.arange(10, 20))
+
+print(np.arange(0, 20, 2))
+
+print(np.arange(0, 10)**2)
+
+print(np.linspace(0, 1, 11))
+
+print(np.array([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3]))
+
+print(t[0])
+print(t[:3])
+print(t[-3:])
